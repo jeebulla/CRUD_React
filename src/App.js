c815b9f2435a9d86a590891eb13637b097173c56
Muhammad Najeeb <yusnaj21@gmail.com>
2023-11-09 17:27:29 +0100

    Initialized: CRUD App React Conversion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="px-5 py-4 max-w-lg mx-auto">
+        <h2 className="text-center my-4 border-b-2 font-bold text-2xl text-slate-500">
+          <span className="text-green-300 text-3xl">Petels</span> TODOIST
+        </h2>
       </header>
+      <main className="my-4 max-w-lg mx-auto p-2">
+        <form className="flex flex-col sm:flex-row justify-between">
+          <input
+            type="text"
+            name="user-input"
+            placeholder="What are you doing today?"
+            className="border py-2 px-4 m-2 sm:w-[80%] rounded-md"
+            id="todo_input"
+          />
+          <button
+            onclick="create_Todo(event)"
+            className="bg-green-300 text-slate-800 py-2 px-4 rounded-md m-2"
+            id="add_todo_btn"
+          >
+            Add&nbsp;Task
+          </button>
+          <button
+            onclick="updateTodo(event)"
+            className="hidden bg-yellow-300 text-slate-800 py-2 px-4 rounded-md m-2"
+            id="update_todo_btn"
+          >
+            Update&nbsp;Task
+          </button>
+        </form>
+        <span id="form_message" className="hidden block text-left p-2" />
+        <div id="task_log" className="bg-green-50 rounded-md p-2" />
+      </main>
     </div>
   );
 }
